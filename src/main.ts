08d93ccf293fdf808c3d1c6a12bea018a0598ef4
Alex Shapovalov <alex@yaplex.com>
2025-01-02 18:51:17 -0500

    trying amplify deployment

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { Amplify } from 'aws-amplify';
+import config from './amplify_outputs.json';
+
+Amplify.configure(config);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
