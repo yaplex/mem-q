4678e7cdb3e72cc90fbbb5accd4d351bc6b5b8f4
Alex Shapovalov <alex@yaplex.com>
2025-01-02 23:01:42 -0500

    Update card component to display images and enhance styling

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -24,7 +24,7 @@ export class BoardComponent implements OnInit {
   }
 
   initializeGame() {
-    const values = ['🌟', '🎈', '🎨', '🎭', '🎪', '🎯', '🎲', '🎱','🐈‍⬛'];
+    const values = ['/assets/1.png', '/assets/2.png', '/assets/3.png', '/assets/4.png', '/assets/5.png', '/assets/6.png', '/assets/7.png', '/assets/8.png','/assets/9.png'];
     this.cards = [...values, ...values].map((value, index) => ({
       id: index,
       value,
